a16d54ff72b14cb430322c1d054038e0b25f9ffd
Vamsi-akkala <akkalavamsi.123@gmail.com>
2022-05-02 22:04:09 +0530

    ss

--- seed.groovy
+++ seed.groovy
@@ -24,3 +24,25 @@ folder('CI-Pipelines') {
             }
         }
     }
+pipelineJob("CI-Pipelines/catalogue-ci") {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+            'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/GDevOps321/frontend-1.git')
+                    }
+                }
+
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                        //'name'('*/tags/*')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
