5fa7f5bec5922a4b9b5a0b87f57fabed9a2cbcd7
Vamsi-akkala <akkalavamsi.123@gmail.com>
2022-05-02 19:22:17 +0530

    ss

---
+++ seed.groovy
@@ -0,0 +1,23 @@
+
+    pipelineJob("frontend-ci") {
+        configure { flowdefinition ->
+            flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+                'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'('https://github.com/GDevOps321/frontend-1.git')
+                        }
+                    }
+
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/main')
+                            //'name'('*/tags/*')
+                        }
+                    }
+                }
+                'scriptPath'('Jenkinsfile')
+                'lightweight'(true)
+            }
+        }
+    }
