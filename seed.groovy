28f04de727486f957f5e5f40877cd839dcf4b291
Vamsi-akkala <akkalavamsi.123@gmail.com>
2022-05-02 22:30:23 +0530

    ss

--- seed.groovy
+++ seed.groovy
@@ -90,3 +90,26 @@ pipelineJob("CI-Pipelines/cart-ci") {
         }
     }
 }
+
+pipelineJob("CI-Pipelines/shipping-ci") {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+            'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/GDevOps321/shipping.git')
+                    }
+                }
+
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                        //'name'('*/tags/*')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
