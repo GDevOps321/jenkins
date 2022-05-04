294bfe167ce2cfc765af437a1ffe55e4e87fdbdd
Vamsi-akkala <akkalavamsi.123@gmail.com>
2022-05-04 19:28:33 +0530

    ss

--- vars/roboshop.groovy
+++ vars/roboshop.groovy
@@ -1,6 +1,6 @@
 def call(Map params  = [:]) {
     def args = [
-            NEXUS_IP  = '172.31.20.130'
+            NEXUS_IP = '172.31.20.130'
     ]
     args << params
 
@@ -10,16 +10,16 @@ def call(Map params  = [:]) {
                     label "${args.SLAVE_LABEL}"
                 }
                 environment {
-                    COMPONENT    = "${args.COMPONENT}"
-                    NEXUS_IP     = "${args.NEXUS_IP}"
+                    COMPONENT = "${args.COMPONENT}"
+                    NEXUS_IP = "${args.NEXUS_IP}"
                     PROJECT_NAME = "${args.PROJECT_NAME}"
-                    SLAVE_LABEL  = "${args.SLAVE_LABEL}"
-                    APP_TYPE     = "${args.APP_TYPE}"
+                    SLAVE_LABEL = "${args.SLAVE_LABEL}"
+                    APP_TYPE = "${args.APP_TYPE}"
                 }
                 stages {
                     stage('prepares artifact') {
                         when {
-                            environment name: 'APP_TYPE' , value: 'NGINX'
+                            environment name: 'APP_TYPE', value: 'NGINX'
                         }
                         steps {
                             sh '''
@@ -28,69 +28,68 @@ def call(Map params  = [:]) {
                            '''
                         }
                     }
-                }
-                stage('Downloading dependencies') {
-                    when {
-                        environment name: 'APP_TYPE' , value: 'NODEJS'
-                    }
-                    steps {
-                        sh '''
+
+                    stage('Downloading dependencies') {
+                        when {
+                            environment name: 'APP_TYPE', value: 'NODEJS'
+                        }
+                        steps {
+                            sh '''
                           npm install
                         '''
+                        }
                     }
-                }
 
-                stage('prepared artifact') {
-                    when {
-                        environment name: 'APP_TYPE' , value: 'JAVA'
+                    stage('prepared artifact') {
+                        when {
+                            environment name: 'APP_TYPE', value: 'JAVA'
+                        }
+                        steps {
+                            sh '''
+                             zip -r ${COMPONENT}.zip node_modules server.js
+                           '''
+                        }
                     }
-                    steps {
-                        sh '''
-                           zip -r ${COMPONENT}.zip node_modules server.js
-                        '''
-                    }
-                }
-                stage('Download dependencies')
-                        {
-                            steps {
-                                sh '''
+                    stage('Download dependencies') {
+                        steps {
+                            sh '''
                                    mvn compile
                                 '''
+                        }
+                    }
+                    stage('Make package') {
+                        steps {
+                            sh '''
+                              mvn package
+                            '''
+                        }
+                    }
+
+                    stage('preparing artifact') {
+                        steps {
+                            sh '''
+                               cp target/*.jar ${COMPONENT}.jar
+                                zip -r ${COMPONENT}.zip ${COMPONENT}.jar
+                            '''
+                        }
+                    }
+                    stage('prepare artifact') {
+                        when {
+                            environment name: 'APP_TYPE', value: 'PYTHON'
+                        }
+                        steps {
+                                  sh '''
+                                     zip -r ${COMPONENT}.zip *
+                                  '''
+                           }
+                       }
+                        stage('upload artifacts to nexus') {
+                          steps {
+                            script {
+                                nexus
                             }
-                        }
-                stage('Make package') {
-                    steps {
-                        sh '''
-                          mvn package
-                        '''
-                    }
-                }
-
-                stage('preparing artifact') {
-                    steps {
-                        sh '''
-                          cp target/*.jar ${COMPONENT}.jar
-                           zip -r ${COMPONENT}.zip ${COMPONENT}.jar
-
-                         '''
-                    }
-                }
-                stage('prepare artifact') {
-                    when {
-                        environment name: 'APP_TYPE' , value: 'PYTHON'
-                    }
-                    steps {
-                        sh '''
-                           zip -r ${COMPONENT}.zip *
-                         '''
-                    }
-                }
-                stage('upload artifacts to nexus') {
-                    steps {
-                        script {
-                            nexus
-                        }
-                    }
+                          }
+                       }
                 }
             }
     }
