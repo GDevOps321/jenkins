b3bb9b955b97b7d2d66dd76b9e362dcacde4bcb6
Vamsi-akkala <akkalavamsi.123@gmail.com>
2022-05-04 19:49:16 +0530

    ss

--- vars/roboshop.groovy
+++ vars/roboshop.groovy
@@ -1,24 +1,25 @@
-def call(Map params  = [:]) {
+def call(Map params = [:]) {
+
     def args = [
-            NEXUS_IP = '172.31.20.130'
+            NEXUS_IP               : '172.31.20.130',
     ]
     args << params
+    pipeline {
+        agent {
+            label "${args.SLAVE_LABEL}"
+        }
 
-    pipeline
-            {
-                agent {
-                    node {
-                        label "${args.SLAVE_LABEL}"
-                    }
-                }
-                environment {
-                    COMPONENT = "${args.COMPONENT}"
-                    NEXUS_IP = "${args.NEXUS_IP}"
-                    PROJECT_NAME = "${args.PROJECT_NAME}"
-                    SLAVE_LABEL = "${args.SLAVE_LABEL}"
-                    APP_TYPE = "${args.APP_TYPE}"
-                }
-                stages {
+
+
+        environment {
+            COMPONENT     = "${args.COMPONENT}"
+            NEXUS_IP      = "${args.NEXUS_IP}"
+            PROJECT_NAME  = "${args.PROJECT_NAME}"
+            SLAVE_LABEL   = "${args.SLAVE_LABEL}"
+            APP_TYPE      = "${args.APP_TYPE}"
+        }
+
+        stages {
                     stage('prepares artifact') {
                         when {
                             environment name: 'APP_TYPE', value: 'NGINX'
