a64d9fe1ce1ff5f53dd316152b0e34d9f52d1bcf
Vamsi-akkala <akkalavamsi.123@gmail.com>
2022-05-05 19:52:46 +0530

    ss

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -30,7 +30,7 @@ def code_build(APP_TYPE, COMPONENT) {
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
     } else if(APP_TYPE == "JAVA") {
-        command = "mvn clean package"
+        command = "mvn package"
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
     }
