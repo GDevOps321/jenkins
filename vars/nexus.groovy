f36bef901ce7fac8ad29526222af89823537157f
Vamsi-akkala <akkalavamsi.123@gmail.com>
2022-05-10 17:19:44 +0530

    ss

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -5,7 +5,7 @@ def nexus1(COMPONENT) {
 
     command = "curl -f -v -u admin:admin --upload-file ${FILENAME} http://172.31.20.130:8081/repository/${COMPONENT}/${FILENAME}"
     def execute_state=sh(returnStdout: true, script: command)
-    manager.addShortText("deployed")
+    //manager.addShortText("deployed")
 }
 
 def make_artifacts(APP_TYPE, COMPONENT) {
